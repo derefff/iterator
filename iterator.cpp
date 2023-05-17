fbc4bf6aaa9ce615c798d22619dfa08ff81da233
derefff <deref122@gmail.com>
2023-05-17 16:42:33 +0200

    created class iterator

---
+++ iterator.cpp
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+class Iterator
+{
+private:
+    int current_index;
+    int previous_index;
+    int next_index;
+
+    int go_next_index()
+    {
+
+    }
+
+    int go_previous_index()
+    {
+        
+    }
+public:
+
+    Iterator()
+    {
+        printf("iterator construct creation");
+    }
+
+
+};
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "iterator.cpp"
+
+int main()
+{
+	Iterator i;
+	
+	return 0;
+}
